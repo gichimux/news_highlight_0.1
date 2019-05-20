abeb673feb8d5d62c7e0684f298d37846f9972f8
gichimux <gichimumwai1@gmail.com>
2019-05-20 17:38:40 +0300

    created search functionality

--- app/requests.py
+++ app/requests.py
@@ -80,3 +80,19 @@ def process_results(news_list):
         news_results.append(news_object)
 
     return news_results
+
+
+def search_news(query):
+    search_news_url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(query,api_key)
+    
+    with urllib.request.urlopen(search_news_url) as url:
+        search_news_data = url.read()
+        search_news_response = json.loads(search_news_data)
+
+        search_news_results = None
+
+        if search_news_response['articles']:
+            search_news_list = search_news_response['articles']
+            search_news_results = process_article_results(search_news_list)
+    
+    return search_news_results
