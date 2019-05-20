0a150859a1074acdf6933479db3d1a2e0a8de31b
gichimux <gichimumwai1@gmail.com>
2019-05-20 10:14:57 +0300

    created tests for article class

--- tests/article_test.py
+++ tests/article_test.py
@@ -5,12 +5,20 @@ class TestArticle(unittest.TestCase):
     '''
     test the behaviour of the article class
     '''
-    
     def setUp(self):
         '''
         test that will run before every test
         '''
-        self.new_article = Article()
+        self.new_article = Article('source','http://img.com','doe', 'article','some article', '2018-05-12T13:31:03Z', 'bbc.com')
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_article 
+    def test_init(self):
+        '''
+        test if object is instantiated correctly
+        '''
+        self.assertEqual(self.new_article.source, "source")
+        self.assertEqual(self.new_article.urlToImage, "http://img.com")
+        self.assertEqual(self.new_article.author, "doe")
+        self.assertEqual(self.new_article.title, "article")
+        self.assertEqual(self.new_article.description, "some article")
+        self.assertEqual(self.new_article.publishedAt, "2018-05-12T13:31:03Z")
+        self.assertEqual(self.new_article.ulr, "bbc.com")
