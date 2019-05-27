2d18e9cc5021f98db3cb44c884ce5bf5e1176fa7
gichimux <gichimumwai1@gmail.com>
2019-05-27 15:00:07 +0300

    push changes to heroku

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from app import create_app
 from flask_script import Manager,Server
 
 #create an app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager .add_command('server', Server)
